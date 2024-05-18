f587ee00e0e849e24fc9d6875498f0cb2e787ee9
Polina <phlejnas009@mail.ru>
2024-05-19 02:02:49 +0300

    Fixed two parts of screen

--- calendar_app/lib/widgets/screen_widget.dart
+++ calendar_app/lib/widgets/screen_widget.dart
@@ -1,106 +1,101 @@
 import 'package:calendar_app/static/colors.dart';
-import 'package:calendar_app/widgets/calendar.dart';
-import 'package:calendar_app/widgets/line_widget.dart';
+import 'package:calendar_app/widgets/screen%20parts/screen_bottom.dart';
+import 'package:calendar_app/widgets/screen%20parts/screen_top.dart';
 import 'package:calendar_app/widgets/sidebar.dart';
-import 'package:calendar_app/widgets/switcher_widget.dart';
-import 'package:calendar_app/widgets/tasks_list.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-import 'textField_widget.dart';
+
 
 class ScreenWidget extends StatelessWidget {
-  ScreenWidget({super.key});
-  final String chosenCal = "Month";
+
+  ScreenWidget({super.key,});
 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset : false,
-        drawer: SideBarWidget(),
-        appBar: AppBar(
-          title: const Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(right: 30),
-                child: Text(
-                  "May 2024",
-                  style: TextStyle(
-                    fontFamily: "Mulish-VariableFont_wght",
-                    fontWeight: FontWeight.w900,
-                    fontSize: 18,
-                    color: Colors.white,
+
+    return  Scaffold(
+          resizeToAvoidBottomInset : false,
+          drawer: SideBarWidget(),
+          appBar: AppBar(
+            title: const Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 30),
+                  child: Text(
+                    "May 2024",
+                    style: TextStyle(
+                      fontFamily: "Mulish-VariableFont_wght",
+                      fontWeight: FontWeight.w900,
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-              )),
-          backgroundColor: CalendarColors.mainBrown,
-          leading: Padding(
-              padding: EdgeInsets.only(left: 30),
-              child: Builder(
-                builder: (context) {
-                  return IconButton(
-                  onPressed: () => Scaffold.of(context).openDrawer(),
-                  icon: const Icon(
-                    Icons.menu,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                );
-                }
-              )),
-        ),
-        body: Column(
-          children: [
-            SwitcherWidget(),
-            TextFieldWidget(),
-            CalendarWidget(),
-            LineWidget(),
-            TasksList(),
-          ],
-        ),
-        floatingActionButton: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 25),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: CalendarColors.darkBrown,
-              shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-            ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: Text("Create new task",
-                            style: TextStyle(
-                              fontFamily: "Mulish-VariableFont_wght",
-                              fontWeight: FontWeight.w900,
-                              fontSize: 15,
-                              color: Colors.white,
-                            ))),
-                            Stack(children: [
-                              Icon(
-                              Icons.circle,
-                              color: CalendarColors.circleBrown,
-                              size: 40,
-                            ),
-                               Icon(
-                              Icons.add,
-                              color: CalendarColors.darkBrown,
-                              size: 40,
-                               )
-                            ])
-                  ],
-                ),
-              ),
-            onPressed: () {
-              Navigator.pushNamed(context, '/new_task');
-            },
+                )),
+            backgroundColor: CalendarColors.mainBrown,
+            leading: Padding(
+                padding: EdgeInsets.only(left: 30),
+                child: Builder(
+                  builder: (context) {
+                    return IconButton(
+                    onPressed: () => Scaffold.of(context).openDrawer(),
+                    icon: const Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  );
+                  }
+                )),
           ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      );
-  }
-}
+          body: const Column(
+            children: [
+              ScreenTopWidget(),
+              ScreenBottomWidget()
+            ],
+          ),
+          floatingActionButton: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 25),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: CalendarColors.darkBrown,
+                shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+              ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Text("Create new task",
+                              style: TextStyle(
+                                fontFamily: "Mulish-VariableFont_wght",
+                                fontWeight: FontWeight.w900,
+                                fontSize: 15,
+                                color: Colors.white,
+                              ))),
+                              Stack(children: [
+                                Icon(
+                                Icons.circle,
+                                color: CalendarColors.circleBrown,
+                                size: 40,
+                              ),
+                                 Icon(
+                                Icons.add,
+                                color: CalendarColors.darkBrown,
+                                size: 40,
+                                 )
+                              ])
+                    ],
+                  ),
+                ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/new_task');
+              },
+            ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        );
+  
+}}
--- calendar_app/lib/widgets/switcher_widget.dart
+++ calendar_app/lib/widgets/switcher_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SwitcherWidget extends StatefulWidget {
+
   const SwitcherWidget({super.key});
 
   @override
@@ -10,6 +11,7 @@ class SwitcherWidget extends StatefulWidget {
 }
 
 class _SwitcherWidgetState extends State<SwitcherWidget> {
+
 
   static const TextStyle switcherTextStyle = TextStyle(
     fontFamily: "Mulish-VariableFont_wght",
@@ -24,11 +26,11 @@ class _SwitcherWidgetState extends State<SwitcherWidget> {
     "Month": CalendarColors.mainBrown,
   };
 
-  // String chosenButton = context.inh;
+  String chosenButton = "Month";
 
   void _changeColor(String name) {
     setState(() {
-    // chosenButton = name;
+    chosenButton = name;
     buttonFill.forEach((key, value) {buttonFill[key] = CalendarColors.lightBrown;});
     buttonFill[name] = CalendarColors.mainBrown;
     });
@@ -37,38 +39,38 @@ class _SwitcherWidgetState extends State<SwitcherWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      color: CalendarColors.mainBrown,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 50, right: 50, bottom: 15),
-        child: Container(
-            padding: const EdgeInsets.only(top: 6, bottom: 6),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              color: CalendarColors.lightBrown,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                for (String name in buttonFill.keys)
-                  TextButton(
-                    onPressed: () {
-                      _changeColor(name);
-                      
-                      },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: buttonFill[name],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+    return  Center(
+          child: Container(
+        color: CalendarColors.mainBrown,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 50, right: 50, bottom: 15),
+          child: Container(
+              padding: const EdgeInsets.only(top: 6, bottom: 6),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: CalendarColors.lightBrown,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  for (String name in buttonFill.keys)
+                    TextButton(
+                      onPressed: () {
+                        _changeColor(name);
+
+                        },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: buttonFill[name],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
                       ),
+                      child: Text(name, style: switcherTextStyle),
                     ),
-                    child: Text(name, style: switcherTextStyle),
-                  ),
-              ],
-            )),
-      ),
-    ));
+                ],
+              )),
+        ),
+      ));
   }
 
 }
